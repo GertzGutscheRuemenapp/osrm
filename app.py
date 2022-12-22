faa7fc2a49649bcf749a0b8c60e8a2b713112b9e
Christoph Franke <franke@ggr-planung.de>
2022-12-22 12:46:10 +0100

    added stop, remove functionality, different build algorithm

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import os, random
 from flask import abort, Flask, request
 from markupsafe import escape
 import subprocess
+import logging
 
 app = Flask(__name__)
+
+DEBUG = True
 
 app.config['DATA_FOLDER'] = os.environ.get('DATA_FOLDER',
                                            os.path.join(os.getcwd(), 'data'))
@@ -20,12 +23,20 @@ PORTS = {
 
 @app.route("/", methods = ['GET'])
 def home():
-    return "OSRM Wrapper"
+    return '''
+    <h2>OSRM Wrapper</h2>
+    <b>modes</b>: 'car' / 'bicycle' / 'foot'
+    <br>
+    POST <b>/build/{mode}</b> to build network (pbf-file in body) <br>
+    POST <b>/run/{mode}</b> to run router <br>
+    POST <b>/remove/{mode}</b> to remove network physically <br>
+    POST <b>/stop/{mode}</b> to stop router <br>
+    '''
 
 @app.route("/build/<mode>", methods = ['POST'])
 def build(mode):
     if mode not in MODES:
-        return ({ 'error': "mode '{}' unknown".format(mode) }, 400)
+        return ({'error': "mode '{}' unknown".format(mode)}, 400)
     file = request.files.get('file')
     if not file or not file.filename:
         return ({ 'error': 'no file provided' }, 400)
@@ -41,28 +52,60 @@ def build(mode):
     for cmd in (
         ['osrm-extract', '-p', fp_lua, fp_pbf],
         ['osrm-partition', fp_osrm],
-        ['osrm-customize', fp_osrm]
+        ['osrm-customize', fp_osrm],
+        ['osrm-contract', fp_osrm]
     ):
         process = subprocess.run(cmd)
         if process.returncode != 0:
-            return ({ 'error': 'Command "{}" failed'.format(cmd[0]) }, 400)
-    return ({ 'message': 'successfully built' }, 200)
+            return ({'error': 'Command "{}" failed'.format(cmd[0])}, 400)
+    msg = 'router "{}" successfully built'.format(mode)
+    logging.info(msg)
+    return ({'message': msg}, 200)
 
 @app.route("/run/<mode>", methods = ['POST'])
 def run(mode):
     if mode not in MODES:
         return ({ 'error': "mode '{}' unknown".format(mode) }, 400)
-    # ports
     fp_osrm = os.path.join(app.config['DATA_FOLDER'], mode + '.osrm')
     if not os.path.exists(fp_osrm):
-        return ({ 'error': "mode '{}' not built yet".format(mode) }, 400)
+        msg = 'error: mode "{}" not built yet'.format(mode)
+        logging.error(msg)
+        return ({'error': msg}, 400)
     if app.process.get(mode):
         app.process[mode].kill()
     body = request.get_json() or {}
     port = body.get('port', PORTS.get(mode, 5000))
+    max_table_size = body.get('max_table_size',
+                              os.environ.get('MAX_TABLE_SIZE', 65535))
     app.process[mode] = subprocess.Popen(
-        ['osrm-routed', '--algorithm', 'mld', '--port', str(port), fp_osrm])
-    return ({ 'message': 'mode "{}" started at port {}'.format(mode, port) }, 200)
+        ['osrm-routed', '--port', str(port),
+         #'--algorithm', 'mld',
+         '--verbosity', 'DEBUG' if DEBUG else 'INFO',
+         '--max-table-size', str(max_table_size), fp_osrm])
+    msg = 'router "{}" started at port {}'.format(mode, port)
+    logging.info(msg)
+    return ({'message': msg}, 200)
+
+@app.route("/remove/<mode>", methods = ['POST'])
+def remove(mode):
+    if app.process.get(mode):
+        app.process[mode].kill()
+    fp_osrm = os.path.join(app.config['DATA_FOLDER'], mode + '.osrm')
+    if os.path.exists(fp_osrm):
+        os.remove(fp_osrm)
+    msg = 'router "{}" removed'.format(fp_osrm)
+    logging.info(msg)
+    return ({'message': msg}, 200)
+
+@app.route("/stop/<mode>", methods = ['POST'])
+def stop(mode):
+    if app.process.get(mode):
+        app.process[mode].kill()
+        msg = 'router "{}" stopped'.format(mode)
+    else:
+        msg = 'router "{}" not running'.format(mode)
+    logging.info(msg)
+    return ({'message': msg}, 200)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001, use_reloader=True)
--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Script that builds or rebuilds the container image using the Dockerfile and requirements.txt
 REM Do not move, rename, or delete this file
-docker build --tag=osrm-docker . %*
+docker build --tag=osrm-flask . %*
