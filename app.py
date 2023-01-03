85b7141245737ed248a2b834461dfc8dddc48e2d
Christoph Franke <franke@ggr-planung.de>
2023-01-03 12:10:24 +0100

    logger and optional algorithm as body param; productive serve

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-import os, random, glob
-from flask import abort, Flask, request
-from markupsafe import escape
+import os, glob
+from flask import Flask, request
 import subprocess
-import logging
-
-DEBUG = True
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
+DEBUG = False
 
 app = Flask(__name__)
-
 
 app.config['DATA_FOLDER'] = os.environ.get('DATA_FOLDER',
                                            os.path.join(os.getcwd(), 'data'))
@@ -24,18 +17,14 @@ PORTS = {
     'foot': os.environ.get('MODE_FOOT_PORT', 5003),
 }
 
-def log_subprocess_output(pipe):
-    for line in iter(pipe.readline, b''):
-        logger.info('got line from subprocess: %r', line)
-
 @app.route("/", methods = ['GET'])
 def home():
     return '''
     <h2>OSRM Wrapper</h2>
     <b>modes</b>: 'car' / 'bicycle' / 'foot'
     <br>
-    POST <b>/build/{mode}</b> to build network (pbf-file in body) <br>
-    POST <b>/run/{mode}</b> to run router <br>
+    POST <b>/build/{mode}</b> to build network (pbf-file in body as 'files', body param 'contract' - true to build with Contract Hierarchies) <br>
+    POST <b>/run/{mode}</b> to run router (body param 'contract' - true to run with Contract Hierarchies) <br>
     POST <b>/remove/{mode}</b> to remove network physically <br>
     POST <b>/stop/{mode}</b> to stop router <br>
     '''
@@ -47,20 +36,18 @@ def build(mode):
     file = request.files.get('file')
     if not file or not file.filename:
         return ({ 'error': 'no file provided' }, 400)
-    # ToDo: how to read body param (form multipart?)
-    #do_contract = request.data.json.get('contract')
-    do_contract = False
+
+    do_contract = request.form.get('contract', '').lower() == 'true'
     if app.process.get(mode):
         app.process[mode].kill()
     data_folder = app.config['DATA_FOLDER']
     if not os.path.exists(data_folder):
         os.mkdir(data_folder)
     for fn in glob.glob(os.path.join(data_folder, '{}*'.format(mode))):
-        logger.info(fn)
         os.remove(fn)
     algorithm_label = 'Contraction Hierarchies' if do_contract \
         else 'Multi-Level Dijkstra'
-    logger.info('Building router for mode "{}" with {} algorithm'.format(
+    print('Building router for mode "{}" with {} algorithm'.format(
         mode, algorithm_label))
     fp_pbf = os.path.join(data_folder, mode + '.pbf')
     fp_osrm = os.path.join(data_folder, mode + '.osrm')
@@ -73,12 +60,12 @@ def build(mode):
         commands.append(['osrm-partition', fp_osrm])
         commands.append(['osrm-customize', fp_osrm])
     for cmd in commands:
-        logger.info('running "{}"'.format(cmd[0]))
+        print('running "{}"'.format(cmd[0]))
         process = subprocess.run(cmd)
         if process.returncode != 0:
             return ({'error': 'Command "{}" failed'.format(cmd[0])}, 400)
     msg = 'router "{}" successfully built'.format(mode)
-    logger.info(msg)
+    print(msg)
     return ({'message': msg}, 200)
 
 @app.route("/run/<mode>", methods = ['POST'])
@@ -86,12 +73,10 @@ def run(mode):
     if mode not in MODES:
         return ({ 'error': "mode '{}' unknown".format(mode) }, 400)
     fp_osrm = os.path.join(app.config['DATA_FOLDER'], mode + '.osrm')
-    # ToDo: how to read body param (form multipart?)
-    #do_contract = request.data.json.get('contract')
-    do_contract = False
+    do_contract = request.form.get('contract', '').lower() == 'true'
     if not os.path.exists(fp_osrm):
         msg = 'error: mode "{}" not built yet'.format(mode)
-        logger.error(msg)
+        print(msg)
         return ({'error': msg}, 400)
     if app.process.get(mode):
         app.process[mode].kill()
@@ -101,27 +86,26 @@ def run(mode):
                               os.environ.get('MAX_TABLE_SIZE', 65535))
     algorithm_label = 'Contraction Hierarchies' if do_contract \
         else 'Multi-Level Dijkstra'
-    msg = 'router "{}" started at port {} with {} algorithm'.format(mode, port, algorithm_label)
+    msg = 'router "{}" started at port {} with {} algorithm'.format(
+        mode, port, algorithm_label)
     app.process[mode] = subprocess.Popen(
         ['osrm-routed', '--port', str(port),
          '--algorithm', 'ch' if do_contract else 'mld',
          '--verbosity', 'DEBUG' if DEBUG else 'INFO',
          '--max-table-size', str(max_table_size), fp_osrm],
         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    with app.process[mode].stdout:
-        log_subprocess_output(app.process[mode].stdout)
-    logger.info(msg)
+    print(msg)
     return ({'message': msg}, 200)
 
 @app.route("/remove/<mode>", methods = ['POST'])
 def remove(mode):
     if app.process.get(mode):
         app.process[mode].kill()
-    fp_osrm = os.path.join(app.config['DATA_FOLDER'], mode + '.osrm')
-    if os.path.exists(fp_osrm):
-        os.remove(fp_osrm)
-    msg = 'router "{}" removed'.format(fp_osrm)
-    logger.info(msg)
+    data_folder = app.config['DATA_FOLDER']
+    for fn in glob.glob(os.path.join(data_folder, '{}*'.format(mode))):
+        os.remove(fn)
+    msg = 'router "{}" removed'.format(mode)
+    print(msg)
     return ({'message': msg}, 200)
 
 @app.route("/stop/<mode>", methods = ['POST'])
@@ -131,11 +115,15 @@ def stop(mode):
         msg = 'router "{}" stopped'.format(mode)
     else:
         msg = 'router "{}" not running'.format(mode)
-    logger.info(msg)
+    print(msg)
     return ({'message': msg}, 200)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8001, use_reloader=True)
+    if DEBUG:
+        app.run(host="0.0.0.0", port=8001, use_reloader=DEBUG)
+    else:
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=8001)
     for mode in MODES:
         run(mode)
 
